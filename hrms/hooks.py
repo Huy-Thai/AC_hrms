61a33be93c76684613a9f4a0497e9497c2227937
huythai <huythai.dev@gmail.com>
2023-04-13 10:47:58 +0700

    update report monthly attendance sheet

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -198,17 +198,17 @@ scheduler_events = {
 	# ],
 	# "weekly": ["hrms.controllers.employee_reminders.send_reminders_in_advance_weekly"],
 	# "monthly": ["hrms.controllers.employee_reminders.send_reminders_in_advance_monthly"],
-  "cron": {
-		"05 08 * * MON-SAT": [
-			"hrms.hr.doctype.employee_checkin.employee_checkin.process_notification_employee_checkin_checkout"
-		],
-		"35 17 * * MON-SAT": [
-			"hrms.hr.doctype.employee_checkin.employee_checkin.process_notification_employee_checkin_checkout"
-		],
-		"15 23 * * MON-SAT": [
-			"hrms.hr.doctype.shift_type.shift_type.process_auto_attendance_daily_for_all_shifts",
-		],
-	}
+ #  "cron": {
+	# 	"05 08 * * MON-SAT": [
+	# 		"hrms.hr.doctype.employee_checkin.employee_checkin.process_notification_employee_checkin_checkout"
+	# 	],
+	# 	"35 17 * * MON-SAT": [
+	# 		"hrms.hr.doctype.employee_checkin.employee_checkin.process_notification_employee_checkin_checkout"
+	# 	],
+	# 	"15 23 * * MON-SAT": [
+	# 		"hrms.hr.doctype.shift_type.shift_type.process_auto_attendance_daily_for_all_shifts",
+	# 	],
+	# }
 }
 
 advance_payment_doctypes = ["Gratuity", "Employee Advance"]
--- hrms/hr/report/monthly_attendance_sheet/monthly_attendance_sheet.py
+++ hrms/hr/report/monthly_attendance_sheet/monthly_attendance_sheet.py
@@ -234,7 +234,7 @@ def get_attendance_map(filters: Filters) -> Dict:
 
 	attendance_list = query.run(as_dict=1)
 	attendance_map = {}
-
+	print(attendance_list)
 	for d in attendance_list:
 		attendance_map.setdefault(d.employee, frappe._dict()).setdefault(d.shift, frappe._dict())
 		attendance_map[d.employee][d.shift][d.day_of_month] = d.status
