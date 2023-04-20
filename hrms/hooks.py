0ccbf5f5f27d09fbaf4232dfeec50550af3a3c4c
huythai <huythai.dev@gmail.com>
2023-04-20 10:08:06 +0700

    config env prod

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -199,7 +199,7 @@ scheduler_events = {
 	# "weekly": ["hrms.controllers.employee_reminders.send_reminders_in_advance_weekly"],
 	# "monthly": ["hrms.controllers.employee_reminders.send_reminders_in_advance_monthly"],
   "cron": {	
-		"0 08 * * MON-SAT": [
+		"0 8 * * MON-SAT": [
 			"hrms.hr.doctype.employee_checkin.employee_checkin.process_notification_employee_with_check_IN"
 		],
 		"30 17 * * MON-SAT": [
--- hrms/hr/doctype/employee_checkin/employee_checkin.py
+++ hrms/hr/doctype/employee_checkin/employee_checkin.py
@@ -319,7 +319,7 @@ def notification_employee_with_logtype(logType):
 	print(logType)
 	print(employeesPass)
 
-	url = "https://acerp-bot-team-dev.pandion.vn/api/notification"
+	url = "https://acerp-bot-team.pandion.vn/api/notification"
 	payload = {"type": "CHECK-IN", "payloads": [json.dumps(notifications)]}
 	print(payload)
 
