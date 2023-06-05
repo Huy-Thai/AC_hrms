cfecf24ef5aefa00a0142c6aa14df6868c20d281
Huy-Thai <huythai.dev@gmail.com>
2023-06-05 16:13:46 +0700

    update datetime func

--- hrms/hr/doctype/shift_type/shift_type.py
+++ hrms/hr/doctype/shift_type/shift_type.py
@@ -231,9 +231,7 @@ class ShiftType(Document):
 		)
 
 		if last_out_log is not None:
-			strp_out_time = dt.strptime(last_out_log.time, "%H:%M")
-			print(strp_out_time)
-			last_out_time = datetime.time(strp_out_time.hour, strp_out_time.minute, 0)
+			last_out_time = datetime.time(last_out_log.hour, last_out_log.minute, 0)
 			print(last_out_time)
 			isMiddayTime = time_in_range(START_MIDDAY, END_MIDDAY, last_out_time)
 			if not isMiddayTime:
